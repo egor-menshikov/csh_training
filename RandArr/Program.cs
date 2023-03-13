a593673f1d3b27567c0a0572bcb6f829fb94060b
Egor Menshikov <r3born.fs@gmail.com>
2023-03-13 13:34:06 +0300

    added RandArr generator

---
+++ RandArr/Program.cs
@@ -0,0 +1,29 @@
+﻿// Declare an array, and fill it with random values.
+
+int[] array = new int[10];
+Console.WriteLine("[{0}]", string.Join(", ", array)); // just showing how it looks after creating.
+
+
+// void RandArr(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0, 100);
+//     }
+//     Console.WriteLine("[{0}]", string.Join(", ", arr));
+// }
+
+// RandArr(array);
+
+
+int[] RandArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+    return arr;
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", RandArr(array)));
+
