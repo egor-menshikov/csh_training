8ea62ac1c85e81b02e525bf033f246cd9c9b15ee
Egor Menshikov <r3born.fs@gmail.com>
2023-03-23 14:42:02 +0300

    upgraded RandArr

--- RandArr/Program.cs
+++ RandArr/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Declare an array, and fill it with random values.
 
-// Генерация с аргументами внутри
+// Генерация
 int[] RandArr()
 {
     Console.Write("Enter array length: ");
@@ -21,34 +21,48 @@ int[] RandArr()
 // Вывод
 void PrintArr(int[] arr)
 {
-    Console.WriteLine();
+    Console.WriteLine(String.Empty);
     for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"{arr[i]} ");
         
     }
-    Console.WriteLine();
-    // Console.WriteLine("[{0}]", string.Join(", ", arr));
+    Console.WriteLine(String.Empty);
+    Console.WriteLine(String.Empty);
 }
 
 // Вызов методов
 int[] array = RandArr();
 PrintArr(array);
 
-// Генерация с аргументами снаружи
-Console.Write("Enter array length: ");
-int len = int.Parse(Console.ReadLine()!);
-Console.Write("Enter minimal range value: ");
-int min = int.Parse(Console.ReadLine()!);
-Console.Write("Enter maximum range value: ");
-int max = int.Parse(Console.ReadLine()!);
-
-int[] RandArr(int len, int min, int max)
-{         
-    int[] arr = new int[len];
+// Генерация double
+double[] RandArr()
+{
+    Console.WriteLine("Generating an array of float numbers:");
+    Console.Write("Enter array length: ");
+    int len = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter minimal range value: ");
+    double min = double.Parse(Console.ReadLine()!);
+    Console.Write("Enter maximum range value: ");
+    double max = double.Parse(Console.ReadLine()!);
+    
+    double[] arr = new double[len];
     for (int i = 0; i < len; i++)
     {
-        arr[i] = new Random().Next(min, max + 1);
+        arr[i] = new Random().NextDouble() * (max - min) + min;
     }
     return arr;
+}
+
+// Вывод double
+void PrintArr(double[] arr)
+{
+    Console.WriteLine(String.Empty);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+        
+    }
+    Console.WriteLine(String.Empty);
+    Console.WriteLine(String.Empty);
 }
