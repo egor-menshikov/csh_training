e39b5cba6dd9dc7dafa8997c2f6f6b9e2a93572c
Egor Menshikov <r3born.fs@gmail.com>
2023-04-18 17:49:04 +0300

    test done

---
+++ remove_row_column/Program.cs
@@ -0,0 +1,94 @@
+﻿//Задайте двумерный массив из целых чисел. Напишите программу,
+//которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine()!);
+Console.Write("The range of random numbers WITH -> ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("The range of random numbers UP TO -> ");
+int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int[,] matrix = new int[row, column];
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillMatrix(int[,] matr, int from, int to)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(from, to + 1);
+        }
+    }
+}
+
+int[] FindMinElement(int[,] matr)
+{
+    int rows = matr.GetLength(0);
+    int columns = matr.GetLength(1);
+    int min_num = matr[0, 0];
+    int[] indexes = new int[2];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            if (min_num > matr[i, j])
+            {
+                min_num = matr[i, j];
+                indexes[0] = i;
+                indexes[1] = j;
+            }
+    }
+    Console.WriteLine(matr[indexes[0], indexes[1]]);
+    return indexes;
+}
+
+int[,] WithoutRowColumn(int[,] matr, int[] min_indexes)
+{
+    int k = 0;
+    int p = 0;
+    int rows = matr.GetLength(0);
+    int columns = matr.GetLength(1);
+    int[,] new_matr = new int[rows - 1, columns - 1];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (min_indexes[0] != i && min_indexes[1] != j)
+            {
+                new_matr[k, p] = matr[i, j];
+                p += 1;
+            }
+        }
+        p = 0;
+        if (min_indexes[0] != i) k += 1;
+    }
+    return new_matr;
+}
+
+Console.WriteLine("The random matrix is:");
+FillMatrix(matrix, start, stop);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+Console.WriteLine("The minimum element of matrix is:");
+int[] minCoord = FindMinElement(matrix);
+
+Console.WriteLine("The new matrix is:");
+int[,] answer = WithoutRowColumn(matrix, minCoord);
+PrintMatrix(answer);
---
+++ test/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый
+// массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив
+// можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении
+// не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+// Метод вывода массива
+void PrintArr(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length - 1)
+            Console.Write($"{arr[i]}");
+        else
+            Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine("]");
+}
+
+// Метод создания нового массива с длиной, равной количеству элементов из <=3 символов
+string[] ThreeCharFilter(string[] arr)
+{
+    // Считаем элементы длиной <=3 символов
+    int count = 0;
+    for (int c = 0; c < arr.Length; c++)
+    {
+        if (arr[c].Length <= 3)
+            count++;
+    }
+
+    // Создаем новый массив длины count и заполняем его
+    string[] result = new string[count];
+    int shift = 0;
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = i + shift; j < arr.Length; j++)
+        {
+            if (arr[j].Length <= 3)
+            {
+                result[i] = arr[j];
+                break;                
+            }
+            else shift++;
+        }
+    }
+    return result;
+}
+
+// Входные массивы
+string[] str1 = { "Hi", "Hello", "2", "5", "cat", "world", ":-)", "horse", "white", "sun" };
+string[] str2 = {"1234", "1567", "-2", "computer science"};
+string[] str3 = {"Russia", "Denmark", "Kazan"};
+
+// Запускаем
+Console.WriteLine("================================================================");
+Console.WriteLine("input:");
+PrintArr(str1);
+string[] answer1 = ThreeCharFilter(str1);
+Console.WriteLine("output:");
+PrintArr(answer1);
+
+Console.WriteLine("================================================================");
+Console.WriteLine("input:");
+PrintArr(str2);
+string[] answer2 = ThreeCharFilter(str2);
+Console.WriteLine("output:");
+PrintArr(answer2);
+
+Console.WriteLine("================================================================");
+Console.WriteLine("input:");
+PrintArr(str3);
+string[] answer3 = ThreeCharFilter(str3);
+Console.WriteLine("output:");
+PrintArr(answer3);
